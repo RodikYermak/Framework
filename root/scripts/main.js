e88479125860d3be1364c69c822489c7c849d5b9
RodikYermak <rodion.yermakov.sd@gmail.com>
2019-05-29 18:33:35 -0700

    Navigation

--- root/scripts/main.js
+++ root/scripts/main.js
@@ -3,3 +3,21 @@ $(document).ready(function() {
     $(this).hide();
   });
 });
+
+// Menu toggle-button
+
+$(document).ready(function() {
+  $('.menu-icon').on('click', function() {
+    $('nav ul').toggleClass('showing');
+  });
+});
+
+// Scrolling Effect
+
+$(window).on('scroll', function() {
+  if ($(window).scrollTop()) {
+    $('nav').addClass('black');
+  } else {
+    $('nav').removeClass('black');
+  }
+});
