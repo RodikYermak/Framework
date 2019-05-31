ef1b37e76585dcab630d846453121a947a9c2708
RodikYermak <rodion.yermakov.sd@gmail.com>
2019-05-31 12:21:32 -0700

    scroll top button

--- root/scripts/main.js
+++ root/scripts/main.js
@@ -33,3 +33,18 @@ function pop() {
     c = 0;
   }
 }
+
+// scroll top button
+$(document).ready(function() {
+  $(window).scroll(function() {
+    if ($(this).scrollTop() > 40) {
+      $('#topBtn').fadeIn();
+    } else {
+      $('#topBtn').fadeOut();
+    }
+  });
+
+  $('#topBtn').click(function() {
+    $('html , body').animate({ scrollTop: 0 }, 800);
+  });
+});
