90cdcc3d1f4204cadcad8b22b4d556514e3d2bb6
RodikYermak <rodion.yermakov.sd@gmail.com>
2019-05-31 11:13:21 -0700

    popup

--- root/scripts/main.js
+++ root/scripts/main.js
@@ -21,3 +21,15 @@ $(window).on('scroll', function() {
     $('nav').removeClass('black');
   }
 });
+
+// popup
+var c = 0;
+function pop() {
+  if (c == 0) {
+    document.getElementById('popup').style.display = 'block';
+    c = 1;
+  } else {
+    document.getElementById('popup').style.display = 'none';
+    c = 0;
+  }
+}
